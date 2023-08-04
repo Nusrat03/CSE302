d98dabd46a31eab22168261c9d7da75f6013d373
Nusrat03 <80265737+Nusrat03@users.noreply.github.com>
2023-08-04 16:23:55 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+import os
+
+from flask import Flask
+from flask.json import JSONEncoder
+from datetime import date
+
+from flaskr import db, auth, products, orders, shopping_cart, shippers
+
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        try:
+            if isinstance(obj, date):
+                return obj.strftime('%Y-%m-%d %H:%M:%S.%f')
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
+
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    DATABASE='flaskr.sqlite',
+)
+
+app.config['JSON_SORT_KEYS'] = False
+app.json_encoder = CustomJSONEncoder
+
+
+@app.route('/index')
+def index():
+    return 'Welcome to our Online Shop!'
+
+
+@app.route('/init-db')
+def initialize_database():
+    db.init_db()
+    return {
+        'message': 'Database Initialized'
+    }
+
+
+db.init_app(app)
+app.register_blueprint(auth.bp)
+app.register_blueprint(products.bp)
+app.register_blueprint(shopping_cart.bp)
+app.register_blueprint(orders.bp)
+app.register_blueprint(shippers.bp)
+app.add_url_rule('/', endpoint='index')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ queries.sql
@@ -0,0 +1,45 @@
+-- SELECT pbc.product_id, p.product_name, pbc.quantity, sci.username, sci.created_at, sci.updated_at, SUM((p.price - p.discount) * pbc.quantity) AS product_total_price
+-- FROM product_by_cart AS pbc
+-- JOIN product AS p
+-- ON pbc.product_id = p.product_id
+-- JOIN shopping_cart_info AS sci
+-- ON sci.cart_id = pbc.cart_id
+-- WHERE pbc.cart_id = 'a59a9625-a108-4c0d-b180-00b5d28798ba'
+-- GROUP BY pbc.product_id
+-- ORDER BY sci.created_at ASC;
+
+
+-- SELECT p.product_id, p.product_name, p.in_stock, pbc.quantity FROM product AS p
+-- JOIN product_by_cart AS pbc
+-- ON p.product_id = pbc.product_id
+-- WHERE pbc.cart_id = 'a59a9625-a108-4c0d-b180-00b5d28798ba';
+
+
+-- SELECT * FROM order_info AS oi
+-- JOIN product_by_cart AS pbc
+-- ON oi.order_id = pbc.cart_id
+-- JOIN shopping_cart_info AS sci
+-- ON pbc.cart_id = sci.cart_id
+-- JOIN product AS p
+-- ON p.product_id = pbc.product_id
+-- WHERE sci.username = 'tanvir';
+
+
+-- SELECT oi.order_id, oi.created_at, oi.payment_method, oi.address, pbc.product_id, pbc.quantity, p.product_name, p.price, SUM((p.price - p.discount) * pbc.quantity) AS product_total_price FROM order_info AS oi
+-- JOIN product_by_cart AS pbc
+-- ON oi.order_id = pbc.cart_id
+-- JOIN shopping_cart_info AS sci
+-- ON pbc.cart_id = sci.cart_id
+-- JOIN product AS p
+-- ON p.product_id = pbc.product_id
+-- WHERE sci.username = 'tanvir'
+
+
+-- SELECT * FROM product
+-- WHERE LOWER(product_name) LIKE '%keyboard%' OR LOWER(description) LIKE '%keyboard%' OR LOWER(product_category) LIKE '%keyboard%' OR
+-- product_id IN (
+--     SELECT product_id FROM product_by_tag WHERE tag_name LIKE '%keyboard%'
+-- );
+
+SELECT product.product_id, product.product_name, product.description, product.product_category, product.price, product.discount FROM product
+WHERE product.product_id IN (SELECT DISTINCT product_wishlist.product_id FROM product_wishlist WHERE product_wishlist.username = 'admin');
